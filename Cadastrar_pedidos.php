57d5b36ef745e9f364e1f170e31c3c4ecbcea049
julio <166665502+Julio-Saless@users.noreply.github.com>
2025-03-31 21:17:08 -0300

    Cadastrar_pedidos.php

---
+++ Cadastrar_pedidos.php
@@ -0,0 +1,39 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: Login.php");
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar Pedidos</title>
+</head>
+<body>
+    <h1>Cadastrando Pedidos do Professor</h1>
+    <br>
+    <form action="pedidos.txt" method="post">
+        <label for="livro">Titulo do Livro</label>
+        <input type="text" id="livro" name="livro">
+        <br>
+        <label for="autor">Nome do Autor</label>
+        <input type="text" id="autor" name="autor">
+        <br>
+        <label for="editora">Nome da Editora</label>
+        <input type="text" id="editora" name="editora">
+        <br>
+        <label for="ISBN">ISBN</label>
+        <input type="int" id="ISBN" name="ISBN">
+        <br>
+        <br>
+        <input type="submit" value="Cadastrar Pedido">
+    </form>    
+</body>
+</html>
